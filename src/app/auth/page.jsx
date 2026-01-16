def689c7e9110afc42f610def48c586b76fad04d
akumanja58-sudo <akumanja58@gmail.com>
2026-01-16 20:49:42 +0700

    🐛 Fix SSR - disable prerender for Auth page

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,6 +1,9 @@
 'use client';
 
-import Auth from '@/components/Auth';
+import dynamic from 'next/dynamic';
+
+// Disable SSR for Auth component
+const Auth = dynamic(() => import('@/components/Auth'), { ssr: false });
 
 export default function AuthPage() {
     return <Auth />;
